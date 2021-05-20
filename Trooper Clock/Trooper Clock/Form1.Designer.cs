a54c9ec8684bd76ae72f22a240d2189137627eac
Melissa S <msr2236@gmail.com>
2021-05-21 00:05:33 +0200

    Added clock files

---
+++ Trooper Clock/Trooper Clock/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Trooper_Clock
+{
+    partial class TrooperTime
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.EmpireData = new System.Windows.Forms.Label();
+            this.DeathStarText = new System.Windows.Forms.Label();
+            this.LegionData = new System.Windows.Forms.Label();
+            this.BCPST_Data = new System.Windows.Forms.Label();
+            this.BCPST_Text = new System.Windows.Forms.Label();
+            this.EmpireText = new System.Windows.Forms.Label();
+            this.LegionText = new System.Windows.Forms.Label();
+            this.RegimentText = new System.Windows.Forms.Label();
+            this.RegimentData = new System.Windows.Forms.Label();
+            this.DeathStarData = new System.Windows.Forms.Label();
+            this.TrooperUpdate = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // EmpireData
+            // 
+            this.EmpireData.AutoSize = true;
+            this.EmpireData.BackColor = System.Drawing.Color.Black;
+            this.EmpireData.Font = new System.Drawing.Font("Star Jedi", 15.9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmpireData.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.EmpireData.Location = new System.Drawing.Point(346, 43);
+            this.EmpireData.Name = "EmpireData";
+            this.EmpireData.Size = new System.Drawing.Size(75, 84);
+            this.EmpireData.TabIndex = 0;
+            this.EmpireData.Text = "0";
+            this.EmpireData.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // DeathStarText
+            // 
+            this.DeathStarText.AutoSize = true;
+            this.DeathStarText.BackColor = System.Drawing.Color.Black;
+            this.DeathStarText.Font = new System.Drawing.Font("Star Jedi", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeathStarText.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.DeathStarText.Location = new System.Drawing.Point(49, 151);
+            this.DeathStarText.Name = "DeathStarText";
+            this.DeathStarText.Size = new System.Drawing.Size(411, 84);
+            this.DeathStarText.TabIndex = 1;
+            this.DeathStarText.Text = "Death Star:";
+            this.DeathStarText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // LegionData
+            // 
+            this.LegionData.AutoSize = true;
+            this.LegionData.BackColor = System.Drawing.Color.Black;
+            this.LegionData.Font = new System.Drawing.Font("Star Jedi", 15.9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LegionData.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.LegionData.Location = new System.Drawing.Point(1460, 39);
+            this.LegionData.Name = "LegionData";
+            this.LegionData.Size = new System.Drawing.Size(75, 84);
+            this.LegionData.TabIndex = 2;
+            this.LegionData.Text = "0";
+            this.LegionData.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // BCPST_Data
+            // 
+            this.BCPST_Data.AutoSize = true;
+            this.BCPST_Data.BackColor = System.Drawing.Color.Black;
+            this.BCPST_Data.Font = new System.Drawing.Font("Star Jedi", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BCPST_Data.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.BCPST_Data.Location = new System.Drawing.Point(485, 411);
+            this.BCPST_Data.Name = "BCPST_Data";
+            this.BCPST_Data.Size = new System.Drawing.Size(530, 191);
+            this.BCPST_Data.TabIndex = 3;
+            this.BCPST_Data.Text = "00000";
+            this.BCPST_Data.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // BCPST_Text
+            // 
+            this.BCPST_Text.AutoSize = true;
+            this.BCPST_Text.BackColor = System.Drawing.Color.Black;
+            this.BCPST_Text.Font = new System.Drawing.Font("Star Jedi", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BCPST_Text.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.BCPST_Text.Location = new System.Drawing.Point(1058, 411);
+            this.BCPST_Text.Name = "BCPST_Text";
+            this.BCPST_Text.Size = new System.Drawing.Size(477, 191);
+            this.BCPST_Text.TabIndex = 4;
+            this.BCPST_Text.Text = "BCPST";
+            this.BCPST_Text.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // EmpireText
+            // 
+            this.EmpireText.AutoSize = true;
+            this.EmpireText.BackColor = System.Drawing.Color.Black;
+            this.EmpireText.Font = new System.Drawing.Font("Star Jedi", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmpireText.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.EmpireText.Location = new System.Drawing.Point(53, 43);
+            this.EmpireText.Name = "EmpireText";
+            this.EmpireText.Size = new System.Drawing.Size(261, 84);
+            this.EmpireText.TabIndex = 5;
+            this.EmpireText.Text = "Empire:";
+            this.EmpireText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // LegionText
+            // 
+            this.LegionText.AutoSize = true;
+            this.LegionText.BackColor = System.Drawing.Color.Black;
+            this.LegionText.Font = new System.Drawing.Font("Star Jedi", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LegionText.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.LegionText.Location = new System.Drawing.Point(1174, 42);
+            this.LegionText.Name = "LegionText";
+            this.LegionText.Size = new System.Drawing.Size(256, 84);
+            this.LegionText.TabIndex = 6;
+            this.LegionText.Text = "Legion:";
+            this.LegionText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // RegimentText
+            // 
+            this.RegimentText.AutoSize = true;
+            this.RegimentText.BackColor = System.Drawing.Color.Black;
+            this.RegimentText.Font = new System.Drawing.Font("Star Jedi", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegimentText.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.RegimentText.Location = new System.Drawing.Point(308, 411);
+            this.RegimentText.Name = "RegimentText";
+            this.RegimentText.Size = new System.Drawing.Size(171, 191);
+            this.RegimentText.TabIndex = 7;
+            this.RegimentText.Text = "R";
+            this.RegimentText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // RegimentData
+            // 
+            this.RegimentData.AutoSize = true;
+            this.RegimentData.BackColor = System.Drawing.Color.Black;
+            this.RegimentData.Font = new System.Drawing.Font("Star Jedi", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegimentData.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.RegimentData.Location = new System.Drawing.Point(54, 411);
+            this.RegimentData.Name = "RegimentData";
+            this.RegimentData.Size = new System.Drawing.Size(260, 191);
+            this.RegimentData.TabIndex = 8;
+            this.RegimentData.Text = "00";
+            this.RegimentData.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // DeathStarData
+            // 
+            this.DeathStarData.AutoSize = true;
+            this.DeathStarData.BackColor = System.Drawing.Color.Black;
+            this.DeathStarData.Font = new System.Drawing.Font("Star Jedi", 15.9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeathStarData.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.DeathStarData.Location = new System.Drawing.Point(489, 151);
+            this.DeathStarData.Name = "DeathStarData";
+            this.DeathStarData.Size = new System.Drawing.Size(75, 84);
+            this.DeathStarData.TabIndex = 9;
+            this.DeathStarData.Text = "0";
+            this.DeathStarData.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // TrooperUpdate
+            // 
+            this.TrooperUpdate.Interval = 31200;
+            this.TrooperUpdate.Tick += new System.EventHandler(this.TrooperUpdate_Tick);
+            // 
+            // TrooperTime
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.BackgroundImage = global::Trooper_Clock.Properties.Resources.Galaxy;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1579, 969);
+            this.Controls.Add(this.DeathStarData);
+            this.Controls.Add(this.RegimentData);
+            this.Controls.Add(this.RegimentText);
+            this.Controls.Add(this.LegionText);
+            this.Controls.Add(this.EmpireText);
+            this.Controls.Add(this.BCPST_Text);
+            this.Controls.Add(this.BCPST_Data);
+            this.Controls.Add(this.LegionData);
+            this.Controls.Add(this.DeathStarText);
+            this.Controls.Add(this.EmpireData);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "TrooperTime";
+            this.Text = "Trooper Time";
+            this.Load += new System.EventHandler(this.TrooperTime_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label EmpireData;
+        private System.Windows.Forms.Label DeathStarText;
+        private System.Windows.Forms.Label LegionData;
+        private System.Windows.Forms.Label BCPST_Data;
+        private System.Windows.Forms.Label BCPST_Text;
+        private System.Windows.Forms.Label EmpireText;
+        private System.Windows.Forms.Label LegionText;
+        private System.Windows.Forms.Label RegimentText;
+        private System.Windows.Forms.Label RegimentData;
+        private System.Windows.Forms.Label DeathStarData;
+        private System.Windows.Forms.Timer TrooperUpdate;
+    }
+}
+
---
+++ Trooper Clock/Trooper Clock/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Trooper_Clock
+{
+    public partial class TrooperTime : Form
+    {
+        ulong troopers;
+        ulong squads;
+        ulong platoons;
+        ulong companies;
+        ulong battalions;
+        ulong regiments;
+        ulong legions;
+        ulong deathStars;
+        ulong empire;
+        ulong displayRegiments;
+
+        private DateTime beginning = new DateTime(2021, 5, 20, 1, 6, 0);
+        //private DateTime beginning = new DateTime(2001, 3, 18, 11, 7, 0);
+        //private DateTime beginning = new DateTime(1997, 12, 17, 11, 7, 0);
+
+        private int timeTillTick;
+
+        public TrooperTime()
+        {
+            InitializeComponent();
+        }
+
+        private void TrooperUpdate_Tick(object sender, EventArgs e)
+        {
+            troopers++;
+            CalculateUnits();
+            UpdateTime();
+        }
+
+        private void TrooperTime_Load(object sender, EventArgs e)
+        {
+            //Get the tim
+            //Convert to troopers
+            SetTimeStart();
+            UpdateTime();
+            timeTillTick = 31200 - (int) CalculateTickOffset();
+
+            //Start timer at the right time
+            Thread.Sleep(timeTillTick);
+            TrooperUpdate.Start();
+            troopers++;
+            CalculateUnits();
+            UpdateTime();
+        }
+
+        private void CalculateUnits()
+        {
+            if(troopers >= 10)
+            {
+                squads++;
+                troopers %= 10;
+            }
+            if(squads >= 5)
+            {
+                platoons++;
+                squads %= 5;
+            }
+            if(platoons >= 4)
+            {
+                companies++;
+                platoons %= 4;
+            }
+            if(companies >= 4)
+            {
+                battalions++;
+                companies %= 4;
+            }
+            if(battalions >= 4)
+            {
+                regiments++;
+                displayRegiments++;
+                battalions %= 4;
+            }
+            if(regiments >= 4)
+            {
+                legions++;
+                regiments %= 4;
+            }
+            if(displayRegiments >= 10)
+            {
+                deathStars++;
+            }
+            if(deathStars >= 9)
+            {
+                //Error probably here
+                empire++;
+                deathStars %= 9;
+                legions = 0;
+                regiments = 0;
+                displayRegiments = 0;
+                battalions = 0;
+                companies = 0;
+                platoons = 0;
+                squads = 0;
+                troopers = 0;
+            }
+        }
+        private void UpdateTime()
+        {
+            BCPST_Data.Text = battalions.ToString() + companies.ToString() + platoons.ToString() + squads.ToString() + troopers.ToString();
+            RegimentData.Text = displayRegiments.ToString();
+            LegionData.Text = legions.ToString();
+            DeathStarData.Text = deathStars.ToString();
+            EmpireData.Text = empire.ToString();
+        }
+
+        private ulong CalculateTickOffset()
+        {
+            DateTime now = DateTime.UtcNow;
+            TimeSpan timePassed = now - beginning;
+            ulong timePassedMil = (ulong)timePassed.TotalMilliseconds;
+            ulong trooperOffset = timePassedMil % 31200;
+
+            return trooperOffset;
+        }
+
+        private ulong CalculateStartTroopers()
+        {
+            DateTime now = DateTime.UtcNow;
+            TimeSpan timePassed = now - beginning;
+            ulong timePassedMil = (ulong)timePassed.TotalMilliseconds;
+            ulong totalTroopers = timePassedMil / 31200;
+
+            return totalTroopers;
+        }
+        private void SetTimeStart()
+        {
+            troopers = CalculateStartTroopers();
+
+            if (troopers >= 10)
+            {
+                squads = troopers/10;
+                troopers %= 10;
+            }
+            if (squads >= 5)
+            {
+                platoons = squads/5;
+                squads %= 5;
+            }
+            if (platoons >= 4)
+            {
+                companies = platoons/4;
+                platoons %= 4;
+            }
+            if (companies >= 4)
+            {
+                battalions = companies/4;
+                companies %= 4;
+            }
+            if (battalions >= 4)
+            {
+                regiments = battalions/4;
+                displayRegiments = battalions / 4;
+                battalions %= 4;
+            }
+            if (regiments >= 4)
+            {
+                legions = regiments/4;
+                regiments %= 4;
+            }
+            if (displayRegiments >= 10)
+            {
+                deathStars = displayRegiments/10;
+                displayRegiments %= 10;
+                legions = displayRegiments / 4;
+            }
+            if (deathStars >= 9)
+            {
+                empire = deathStars / 9;
+                deathStars %= 9;
+            }
+        }
+
+    }
+}
